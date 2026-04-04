0d8e579d906046c1e278779bd0035dc7c0a251f8
azanenghi <andrea.zanenghi@gmail.com>
2026-04-05 00:20:47 +0200

    feat(docs): add RadiantComponentCounter eco.component wrapper

---
+++ apps/docs/src/components/radiant-counter/radiant-component-counter.tsx
@@ -0,0 +1,13 @@
+import { eco } from '@ecopages/core';
+import type { RadiantCounterProps } from './radiant-component-counter.script';
+import './radiant-component-counter.script';
+
+export const RadiantComponentCounter = eco.component({
+	dependencies: {
+		scripts: ['./radiant-component-counter.script.tsx'],
+		stylesheets: ['./radiant-counter.css'],
+	},
+	render(props: RadiantCounterProps) {
+		return <radiant-counter prop:value={props.value} />;
+	},
+});
