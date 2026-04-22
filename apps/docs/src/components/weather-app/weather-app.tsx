fab56999d904a40a71916e89e41bf4d612ed2f97
azanenghi <andrea.zanenghi@gmail.com>
2026-04-22 21:28:35 +0200

    fix(weather-app): update WeatherApp component to use self-closing tag

--- apps/docs/src/components/weather-app/weather-app.tsx
+++ apps/docs/src/components/weather-app/weather-app.tsx
@@ -1,6 +1,7 @@
 import type { EcoComponent } from '@ecopages/core';
+import './weather-app.script';
 
-export const WeatherApp: EcoComponent = () => <radiant-weather-app />;
+export const WeatherApp: EcoComponent = () => <radiant-weather-app></radiant-weather-app>;
 
 WeatherApp.config = {
 	dependencies: {
