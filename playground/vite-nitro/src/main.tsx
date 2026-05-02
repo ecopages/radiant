596aacdfd52162a73a1146afa4df515faf02109d
azanenghi <andrea.zanenghi@gmail.com>
2026-05-02 11:53:35 +0200

    fix(playground): hydrate nitro app before starting controllers

--- playground/vite-nitro/src/main.tsx
+++ playground/vite-nitro/src/main.tsx
@@ -1,5 +1,5 @@
 import '@ecopages/radiant/client/install-hydrator';
-import { startControllers } from '@ecopages/radiant/controller-registry';
+import { startControllers } from '@ecopages/radiant';
 import './components/radiant-counter.script';
 import './components/radiant-controller-context-visualizer.script';
 import './components/radiant-controller-decorator-visualizer.script';
@@ -11,7 +11,6 @@ import './style.css';
 import { createRoot } from '@ecopages/jsx';
 import { App } from './app';
 
-startControllers(document);
-
 const root = createRoot(document.getElementById('app')!);
-root.render(<App />);
+root.hydrate(<App />);
+startControllers(document);
