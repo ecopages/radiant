edb9cfab3d48f061ff66931abe743f4d7d8ae235
azanenghi <andrea.zanenghi@gmail.com>
2026-08-02 23:40:40 +0200

    feat(radiant-ui): add date-range-picker component
    Add date range picker with calendar popup.

---
+++ packages/radiant-ui/src/components/ui/date-range-picker/index.ts
@@ -0,0 +1,6 @@
+export {
+	RuiDateRangePicker as RuiDateRangePickerElement,
+	type RuiDateRangePickerProps,
+	type RuiDateRangePickerChangeDetail,
+} from './date-range-picker.script';
+export { RuiDateRangePicker } from './date-range-picker';
