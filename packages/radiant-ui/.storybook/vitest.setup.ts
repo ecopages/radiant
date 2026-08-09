3cd7ee1fcc078d89114dc8461d6209e3181c12de
azanenghi <andrea.zanenghi@gmail.com>
2026-08-10 00:29:32 +0200

    fix(radiant-ui-docs): repaint SSR playgrounds and stack select label

--- packages/radiant-ui/.storybook/vitest.setup.ts
+++ packages/radiant-ui/.storybook/vitest.setup.ts
@@ -1,8 +1,1 @@
-import { beforeAll } from 'vitest';
-import { setProjectAnnotations } from '@ecopages/storybook-radiant-vite';
 import '@ecopages/radiant/client/install-hydrator';
-import * as projectAnnotations from './preview';
-
-const annotations = setProjectAnnotations([projectAnnotations]);
-
-beforeAll(annotations.beforeAll);
